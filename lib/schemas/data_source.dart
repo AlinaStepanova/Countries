ad57e7b59c2ecfd65c8e1fe998a5da4c11b06041
AlinaStepanova <astepanova@yukon.cv.ua>
2022-06-27 20:52:59 +0200

    make data source singleton

--- lib/schemas/data_source.dart
+++ lib/schemas/data_source.dart
@@ -3,11 +3,19 @@ import 'package:flutter/foundation.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class DataSource {
+  static final DataSource _dataSource = DataSource._internal();
+  DataSource._internal();
+  factory DataSource() {
+    return _dataSource;
+  }
+
   ValueNotifier<GraphQLClient> getClient() {
-    ValueNotifier<GraphQLClient> _client = ValueNotifier(GraphQLClient(
-      link: HttpLink(baseUrl),
-      cache: GraphQLCache(store: HiveStore()),
-    ));
+    ValueNotifier<GraphQLClient> _client = ValueNotifier(
+      GraphQLClient(
+        link: HttpLink(baseUrl),
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
 
     return _client;
   }
